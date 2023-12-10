887f5f1ccef6147172e5293f3800ed43d9442474
filtys00 <tysfilip@gmail.com>
2023-12-10 10:53:14 +0100

    feat: add remove icon

--- src/main.rs
+++ src/main.rs
@@ -137,6 +137,7 @@ async fn web_server(store: TranslationStore) {
         .route("/icon/search.svg", get(search_icon))
         .route("/icon/language.svg", get(language_icon))
         .route("/icon/loading.svg", get(loading_icon))
+        .route("/icon/remove.svg", get(remove_icon))
         .route("/favicon.ico", get(language_icon))
         .with_state(store);
 
@@ -622,5 +623,16 @@ async fn loading_icon() -> Response {
             HeaderValue::from_static("image/svg+xml"),
         )],
         r#"<svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512"><!--!Font Awesome Free 6.5.1 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2023 Fonticons, Inc.--><path d="M304 48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 416a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM48 304a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm464-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM142.9 437A48 48 0 1 0 75 369.1 48 48 0 1 0 142.9 437zm0-294.2A48 48 0 1 0 75 75a48 48 0 1 0 67.9 67.9zM369.1 437A48 48 0 1 0 437 369.1 48 48 0 1 0 369.1 437z"/></svg>"#,
+    ).into_response()
+}
+
+async fn remove_icon() -> Response {
+    debug!("Request for '/icon/remove.svg'");
+    (
+        [(
+            header::CONTENT_TYPE,
+            HeaderValue::from_static("image/svg+xml"),
+        )],
+        r#"<svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512" fill="white"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"/></svg>"#,
     ).into_response()
 }
